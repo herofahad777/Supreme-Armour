b5fba5afa7b3858cb1db417a14a67f6115ec2f15
herofahad777 <Fahad@asusfahadlaptop.tail5bfa0.ts.net.>
2023-05-11 22:11:55 +0530

    Fixes:- hands movement and double arms were looking. removed:- mod logo swat armour item tex all added:- 3D looking mod logo 3D looking Swat Armour item texture changes:- Swat helmet eye sight location changed

--- src/main/java/net/mcreator/supremearmor/item/SwatItem.java
+++ src/main/java/net/mcreator/supremearmor/item/SwatItem.java
@@ -178,14 +178,14 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 		private final ModelRenderer cube_r10;
 		private final ModelRenderer cube_r11;
 		private final ModelRenderer cube_r12;
-		private final ModelRenderer LeftArm;
-		private final ModelRenderer cube_r13;
-		private final ModelRenderer RightArm;
-		private final ModelRenderer cube_r14;
 		private final ModelRenderer RightLeg;
 		private final ModelRenderer LeftLeg;
 		private final ModelRenderer Leftboot;
 		private final ModelRenderer Rightboot;
+		private final ModelRenderer LeftArm;
+		private final ModelRenderer cube_r13;
+		private final ModelRenderer RightArm;
+		private final ModelRenderer cube_r14;
 
 		public ModelSwat_Armor() {
 			textureWidth = 128;
@@ -208,17 +208,17 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 			cube_r2.setRotationPoint(0.0035F, 28.7841F, -0.3607F);
 			Smoother.addChild(cube_r2);
 			setRotationAngle(cube_r2, -0.6109F, -1.5708F, 0.0F);
-			cube_r2.setTextureOffset(0, 126).addBox(-5.0F, -24.0F, -23.0F, 10.0F, 1.0F, 1.0F, 0.0F, false);
+			cube_r2.setTextureOffset(0, 126).addBox(-4.0F, -24.0F, -23.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
 			cube_r3 = new ModelRenderer(this);
 			cube_r3.setRotationPoint(0.0035F, 28.7841F, -0.3607F);
 			Smoother.addChild(cube_r3);
 			setRotationAngle(cube_r3, -0.6109F, 0.0F, 0.0F);
-			cube_r3.setTextureOffset(0, 126).addBox(-5.0F, -24.0F, -23.0F, 10.0F, 1.0F, 1.0F, 0.0F, false);
+			cube_r3.setTextureOffset(0, 126).addBox(-4.0F, -24.0F, -23.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
 			cube_r4 = new ModelRenderer(this);
 			cube_r4.setRotationPoint(0.0035F, 28.7841F, -0.3607F);
 			Smoother.addChild(cube_r4);
 			setRotationAngle(cube_r4, -0.6109F, 1.5708F, 0.0F);
-			cube_r4.setTextureOffset(0, 126).addBox(-5.0F, -24.0F, -23.0F, 10.0F, 1.0F, 1.0F, 0.0F, false);
+			cube_r4.setTextureOffset(0, 126).addBox(-4.0F, -24.0F, -23.0F, 8.0F, 1.0F, 1.0F, 0.0F, false);
 			cube_r5 = new ModelRenderer(this);
 			cube_r5.setRotationPoint(0.0035F, 28.7841F, -0.3607F);
 			Smoother.addChild(cube_r5);
@@ -270,9 +270,20 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 			Body.addChild(cube_r12);
 			setRotationAngle(cube_r12, 0.0F, 3.1416F, 0.0F);
 			cube_r12.setTextureOffset(0, 112).addBox(3.0F, -24.0F, -2.0F, 1.0F, 12.0F, 4.0F, 0.0F, false);
+			RightLeg = new ModelRenderer(this);
+			RightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
+			RightLeg.setTextureOffset(0, 114).addBox(-3.0F, 0.0F, -3.0F, 5.0F, 8.0F, 6.0F, 0.0F, false);
+			LeftLeg = new ModelRenderer(this);
+			LeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+			LeftLeg.setTextureOffset(0, 114).addBox(-2.0F, 0.0F, -3.0F, 5.0F, 8.0F, 6.0F, 0.0F, false);
+			Leftboot = new ModelRenderer(this);
+			Leftboot.setRotationPoint(1.9F, 12.0F, 0.0F);
+			Leftboot.setTextureOffset(0, 117).addBox(-3.0F, 7.0F, -3.0F, 7.0F, 5.0F, 6.0F, 0.0F, false);
+			Rightboot = new ModelRenderer(this);
+			Rightboot.setRotationPoint(-1.9F, 12.0F, 0.0F);
+			Rightboot.setTextureOffset(0, 117).addBox(-4.0F, 7.0F, -3.0F, 7.0F, 5.0F, 6.0F, 0.0F, false);
 			LeftArm = new ModelRenderer(this);
 			LeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-			Body.addChild(LeftArm);
 			cube_r13 = new ModelRenderer(this);
 			cube_r13.setRotationPoint(-5.0F, 22.0F, 0.0F);
 			LeftArm.addChild(cube_r13);
@@ -286,7 +297,6 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 			cube_r13.setTextureOffset(97, 115).addBox(-9.0F, -24.0F, 2.0F, 5.0F, 12.0F, 1.0F, 0.0F, false);
 			RightArm = new ModelRenderer(this);
 			RightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
-			Body.addChild(RightArm);
 			cube_r14 = new ModelRenderer(this);
 			cube_r14.setRotationPoint(5.0F, 22.0F, 0.0F);
 			RightArm.addChild(cube_r14);
@@ -298,18 +308,6 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 			cube_r14.setTextureOffset(0, 121).addBox(4.0F, -25.0F, -3.0F, 5.0F, 1.0F, 6.0F, 0.0F, false);
 			cube_r14.setTextureOffset(0, 122).addBox(9.0F, -19.0F, -2.0F, 1.0F, 3.0F, 3.0F, 0.0F, false);
 			cube_r14.setTextureOffset(86, 115).addBox(4.0F, -24.0F, 2.0F, 4.0F, 12.0F, 1.0F, 0.0F, false);
-			RightLeg = new ModelRenderer(this);
-			RightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
-			RightLeg.setTextureOffset(0, 114).addBox(-3.0F, 0.0F, -3.0F, 5.0F, 8.0F, 6.0F, 0.0F, false);
-			LeftLeg = new ModelRenderer(this);
-			LeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
-			LeftLeg.setTextureOffset(0, 114).addBox(-2.0F, 0.0F, -3.0F, 5.0F, 8.0F, 6.0F, 0.0F, false);
-			Leftboot = new ModelRenderer(this);
-			Leftboot.setRotationPoint(1.9F, 12.0F, 0.0F);
-			Leftboot.setTextureOffset(0, 117).addBox(-3.0F, 7.0F, -3.0F, 7.0F, 5.0F, 6.0F, 0.0F, false);
-			Rightboot = new ModelRenderer(this);
-			Rightboot.setRotationPoint(-1.9F, 12.0F, 0.0F);
-			Rightboot.setTextureOffset(0, 117).addBox(-4.0F, 7.0F, -3.0F, 7.0F, 5.0F, 6.0F, 0.0F, false);
 		}
 
 		@Override
@@ -321,6 +319,8 @@ public class SwatItem extends SupremeArmourModElements.ModElement {
 			LeftLeg.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 			Leftboot.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 			Rightboot.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+			LeftArm.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+			RightArm.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 		}
 
 		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
