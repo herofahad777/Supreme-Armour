f1270a8245cce7ec49f1fd6ff7887c88cea1c047
herofahad777 <Fahad@asusfahadlaptop.tail5bfa0.ts.net.>
2023-05-23 20:49:52 +0530

    Advancement added. Swat folder added and transfered all swat stuf to it. send message to all player when world loads failed.

---
+++ src/main/java/net/mcreator/supremearmor/procedures/SwatAdvancementProcedureProcedure.java
@@ -0,0 +1,53 @@
+package net.mcreator.supremearmor.procedures;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import net.mcreator.supremearmor.item.SwatItem;
+import net.mcreator.supremearmor.SupremeArmourMod;
+
+import java.util.Map;
+import java.util.Iterator;
+
+public class SwatAdvancementProcedureProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				SupremeArmourMod.LOGGER.warn("Failed to load dependency entity for procedure SwatAdvancementProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (SwatItem.boots == ((entity instanceof LivingEntity)
+				? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.FEET)
+				: ItemStack.EMPTY).getItem()) {
+			if (SwatItem.legs == ((entity instanceof LivingEntity)
+					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.LEGS)
+					: ItemStack.EMPTY).getItem()) {
+				if (SwatItem.helmet == ((entity instanceof LivingEntity)
+						? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.HEAD)
+						: ItemStack.EMPTY).getItem()) {
+					if (entity instanceof ServerPlayerEntity) {
+						Advancement _adv = ((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager()
+								.getAdvancement(new ResourceLocation("supreme_armour:prepare_to_fight"));
+						AdvancementProgress _ap = ((ServerPlayerEntity) entity).getAdvancements().getProgress(_adv);
+						if (!_ap.isDone()) {
+							Iterator _iterator = _ap.getRemaningCriteria().iterator();
+							while (_iterator.hasNext()) {
+								String _criterion = (String) _iterator.next();
+								((ServerPlayerEntity) entity).getAdvancements().grantCriterion(_adv, _criterion);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- src/main/java/net/mcreator/supremearmor/procedures/SwathelemetOverlayDisplayOverlayIngameProcedure.java
+++ src/main/java/net/mcreator/supremearmor/procedures/SwathelemetOverlayDisplayOverlayIngameProcedure.java
@@ -1,10 +1,5 @@
 package net.mcreator.supremearmor.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.TickEvent;
-
-import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.entity.LivingEntity;
@@ -14,30 +9,8 @@ import net.mcreator.supremearmor.item.SwatItem;
 import net.mcreator.supremearmor.SupremeArmourMod;
 
 import java.util.Map;
-import java.util.HashMap;
 
 public class SwathelemetOverlayDisplayOverlayIngameProcedure {
-	@Mod.EventBusSubscriber
-	private static class GlobalTrigger {
-		@SubscribeEvent
-		public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-			if (event.phase == TickEvent.Phase.END) {
-				Entity entity = event.player;
-				World world = entity.world;
-				double i = entity.getPosX();
-				double j = entity.getPosY();
-				double k = entity.getPosZ();
-				Map<String, Object> dependencies = new HashMap<>();
-				dependencies.put("x", i);
-				dependencies.put("y", j);
-				dependencies.put("z", k);
-				dependencies.put("world", world);
-				dependencies.put("entity", entity);
-				dependencies.put("event", event);
-				executeProcedure(dependencies);
-			}
-		}
-	}
 
 	public static boolean executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
